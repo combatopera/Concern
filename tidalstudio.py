22b0a8f7dc414e78e6ba9ec66c1a26853c05893d
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2018-05-07 11:13:51 +0100

    pristine

---
+++ tidalstudio.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env pyven
+
+import tempfile, subprocess, os, sys, aridity
+from aridimpl.model import Function, Text
+
+def main():
+    studiodir = os.path.dirname(os.path.realpath(__file__))
+    args = sys.argv[1:]
+    projectconfname = 'studioproject.arid'
+    if args:
+        projectdir, = args
+        projectconf = os.path.join(os.path.abspath(projectdir), projectconfname)
+    elif os.path.exists(projectconfname):
+        projectconf = os.path.abspath(projectconfname)
+    else:
+        projectconf = os.path.join(studiodir, projectconfname)
+    tidaldir = os.path.dirname(studiodir)
+    configdir = os.path.join(os.path.expanduser('~'), '.tidalstudio')
+    os.makedirs(configdir, exist_ok = True)
+    with tempfile.TemporaryDirectory(dir = configdir) as tempdir:
+        vimrc = os.path.join(tempdir, 'vimrc')
+        boottidal = os.path.join(tempdir, 'BootTidal.hs')
+        sendblock = os.path.join(tempdir, 'sendblock.py')
+        screenrc = os.path.join(tempdir, 'screenrc')
+        context = aridity.Context()
+        def tosamplesdir(context, samples):
+            samplesdir = os.path.join(tempdir, 'Dirt-Samples')
+            os.mkdir(samplesdir)
+            for name, paths in samples.resolve(context).unravel().items():
+                if not hasattr(paths, 'items'): continue # TODO: This is a hack.
+                path = os.path.join(samplesdir, name)
+                os.mkdir(path)
+                for target in paths:
+                    os.symlink(target, os.path.join(path, os.path.basename(target)))
+            return Text(samplesdir)
+        context['tidalstudio', 'toSamplesDir'] = Function(tosamplesdir)
+        with aridity.Repl(context) as repl:
+            printf = repl.printf
+            printf("cd %s", studiodir)
+            printf('. tidalstudio.arid')
+            printf('tidalstudio')
+            printf("\ttidal home = %s", tidaldir)
+            printf("\tvimrcPath = %s", vimrc)
+            printf("\tBootTidal = %s", boottidal)
+            printf("\tsendblock = %s", sendblock)
+            printf("\t. %s", projectconf)
+            printf("redirect %s", vimrc)
+            printf('tidalstudio < vimrc.aridt')
+            printf("redirect %s", boottidal)
+            printf('tidalstudio tidal < BootTidal.hs.aridt')
+            printf("redirect %s", sendblock)
+            printf('" = $(pystr)')
+            printf('tidalstudio < sendblock.py.aridt')
+            printf("redirect %s", screenrc)
+            printf('" = $(screenstr)')
+            printf('tidalstudio < screenrc.aridt')
+        subprocess.check_call(['screen', '-S', context.resolved('tidalstudio', 'sessionName').value, '-c', screenrc])
+
+if '__main__' == __name__:
+    main()
