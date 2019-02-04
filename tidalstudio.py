b54aa8851449eed52110326820a61156c75fd9b5
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-02-04 23:35:05 +0000

    inline a file

--- tidalstudio.py
+++ tidalstudio.py
@@ -1,23 +1,11 @@
 #!/usr/bin/env pyven
 
+from pathlib import Path
 import tempfile, subprocess, os, sys, aridity
 
-studiodir = os.path.dirname(os.path.realpath(__file__))
-
-def projectconfpath(args):
-    projectconfname = 'studioproject.arid'
-    if args:
-        projectdir, = args
-        return os.path.join(os.path.abspath(projectdir), projectconfname)
-    elif os.path.exists(projectconfname):
-        return os.path.abspath(projectconfname)
-    else:
-        return os.path.join(studiodir, projectconfname)
-
 def main():
-    projectconf = projectconfpath(sys.argv[1:])
-    configdir = os.path.join(os.path.expanduser('~'), '.tidalstudio')
-    os.makedirs(configdir, exist_ok = True)
+    configdir = Path.home() / '.tidalstudio'
+    configdir.mkdir(parents = True, exist_ok = True)
     with tempfile.TemporaryDirectory(dir = configdir) as tempdir:
         vimrc = os.path.join(tempdir, 'vimrc')
         sendblock = os.path.join(tempdir, 'sendblock.py')
@@ -25,12 +13,11 @@ def main():
         context = aridity.Context()
         with aridity.Repl(context) as repl:
             printf = repl.printf
-            printf("cd %s", studiodir)
+            printf("cd %s", Path(__file__).resolve().parent)
             printf('. tidalstudio.arid')
             printf('tidalstudio')
             printf("\tvimrcPath = %s", vimrc)
             printf("\tsendblock = %s", sendblock)
-            printf("\t. %s", projectconf)
             printf("redirect %s", vimrc)
             printf('tidalstudio < vimrc.aridt')
             printf("redirect %s", sendblock)
