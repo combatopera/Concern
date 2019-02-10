56c6813b4b6f771b911ae8425311e3de7d773bcf
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2019-02-10 21:02:10 +0000

    unit test wip

---
+++ test_getblock.py
@@ -0,0 +1,38 @@
+# Copyright 2019 Andrzej Cichocki
+
+# This file is part of Concern.
+#
+# Concern is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Concern is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Concern.  If not, see <http://www.gnu.org/licenses/>.
+
+from getblock import getblock
+import unittest
+
+text = '''
+hello
+function with
+ indented block
+class with
+
+\tblock after blank
+\t and its own indented block
+
+\tand back again after a blank
+something else'''
+
+class TestGetBlock(unittest.TestCase):
+
+    def test_works(self):
+        self.assertEqual('# Nothing to send.\xb6\n', getblock(text, 1))
+        self.assertEqual('hello\xb6\n', getblock(text, 2))
+        self.assertEqual('function with\n indented block\xb6\n', getblock(text, 3))
