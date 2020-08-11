a3c65c173eaaee82a5585310b74a8aee0bb96267
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2020-08-11 17:26:50 +0100

    hopefully make it installable again

---
+++ concern/consumer/__init__.py
@@ -0,0 +1,17 @@
+# Copyright 2019 Andrzej Cichocki
+
+# This file is part of Concern.
+#
+# Concern is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Concern is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Concern.  If not, see <http://www.gnu.org/licenses/>.
+
