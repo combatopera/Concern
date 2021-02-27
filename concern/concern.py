f1ee7b3419ac6e28b471fd92fcc6d263c10d29fd
Andrzej Cichocki <andrzej.cichocki@gmail.com>
2021-02-27 10:43:52 +0000

    use pyref

--- concern/concern.py
+++ concern/concern.py
@@ -65,7 +65,6 @@ def main_Concern():
             (-config).printf("vimrc userPath = %s", uservimrc)
         else:
             log.info("No such file: %s", uservimrc)
-        (-config).printf("interpreter = %s", sys.executable) # TODO: Migrate to config file.
         (-config).printf("vimrcPath = %s", concernvimrc)
         (-config).printf("looppath = %s", looppath)
         (-config).printf("sendblock = %s", sendblock)
